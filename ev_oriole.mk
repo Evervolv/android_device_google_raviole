6273f6cd0c70b90f7706e14e28e7ae0209392819
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    raviole: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- ev_oriole.mk
+++ ev_oriole.mk
@@ -30,3 +30,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_THUMBPRINT="13/TQ1A.221205.004/9244662:user/release-keys"
 
 BUILD_FINGERPRINT := google/oriole/oriole:13/TQ1A.221205.011/9244662:user/release-keys
+
+$(call inherit-product, vendor/google/oriole/oriole-vendor.mk)
