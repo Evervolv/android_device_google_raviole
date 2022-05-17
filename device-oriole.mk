e4bd03232c6f068b27b58deefbc4b47e2a34d368
Michael Bestas <mkbestas@gmail.com>
2022-05-18 00:03:07 +0530

    oriole: Add HbmSVManager RRO overlay
    * Based on redfin
    * raven doesn't have it on stock
    
    Change-Id: I54d885c233151498372e1b510284d6a297ac3e94

--- device-oriole.mk
+++ device-oriole.mk
@@ -267,3 +267,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Enable adpf cpu hint session for SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.enable_adpf_cpu_hint=true
+
+# HBM
+PRODUCT_PACKAGES += \
+    HbmSVManagerOverlay
