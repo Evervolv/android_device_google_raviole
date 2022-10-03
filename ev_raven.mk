3397997c46003609e4c5b15581c0bc46b6d5e662
Michael Bestas <mkbestas@gmail.com>
2022-10-04 01:02:58 +0300

    Update fingerprint/build description from TP1A.221005.002
    Change-Id: I97454c3f78603d004aa84c5c8df7e5110b4c0d54

--- ev_raven.mk
+++ ev_raven.mk
@@ -26,9 +26,9 @@ PRODUCT_CODENAME := Acerbus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys" \
-    BUILD_THUMBPRINT="13/TP1A.220905.004/8927612:user/release-keys"
+    PRIVATE_BUILD_DESC="raven-user 13 TP1A.221005.002 9012097 release-keys" \
+    BUILD_THUMBPRINT="13/TP1A.221005.002/9012097:user/release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
