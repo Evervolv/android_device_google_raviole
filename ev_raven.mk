c2ab4c7319fe7d6b32d1978f539a187dda193380
Michael Bestas <mkbestas@lineageos.org>
2023-03-20 22:26:57 +0200

    Update fingerprint/build description from TQ2A.230305.008.E1
    Change-Id: Ie0d6d5adeeb167c6845b2b58bf35bab1e693e53a

--- ev_raven.mk
+++ ev_raven.mk
@@ -27,9 +27,8 @@ PRODUCT_CODENAME := Acerbus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 13 TQ1A.230205.002 9471150 release-keys" \
-    BUILD_THUMBPRINT="13/TQ1A.230205.002/9471150:user/release-keys"
+    PRIVATE_BUILD_DESC="raven-user 13 TQ2A.230305.008.E1 9677224 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:13/TQ1A.230205.002/9471150:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:13/TQ2A.230305.008.E1/9677224:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
